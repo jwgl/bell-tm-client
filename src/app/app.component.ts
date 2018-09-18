4d8111d17d4e4bd94fd65ba345228fd377672391
Yang Lin <todoubaba@gmail.com>
2018-09-19 02:18:30 +0800

    Workaround navbar-expand bug (#15)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'tm-app-root',
-    styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.scss'],
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
